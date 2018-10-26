80b16f267f627f0e95cb77ef4406e8f73b93ed30
bluerumex <bluerumex@naver.com>
2018-10-26 16:09:30 +0900

    head에 countì t 정보 삽입, body 문자열 자르기

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -51,8 +51,28 @@ exports.write = async (ctx) => {
 GET /api/posts
 */
 exports.list = async (ctx) => {
+    const page = parseInt(ctx.query.page || 1, 10);
+
+    if (page < 1) {
+        ctx.status = 400;
+        return;
+    }
+
     try {
-        const posts = await Post.find().exec();
+        const posts = await Post.find()
+            .sort({_id: -1})
+            .limit(10)
+            .skip((page - 1) * 10)
+            .lean()
+            .exec();
+        const postCount = await Post.count().exec();
+        const limitBodyLength = post => ({
+            ...post,
+            body: post.body.lenth < 200 ? 
+                post.body : 
+                `{post.body.slice(0, 200)}...`
+        });
+        ctx.set('Last-page', Math.ceil(postCount / 10));
         ctx.body = posts;
     } catch(e) {
         ctx.throw(e, 500);
